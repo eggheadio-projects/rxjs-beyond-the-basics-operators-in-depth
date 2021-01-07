23570f27a481565ec1f732f987c8aa8adab3b385
lsminter <lsminteris@gmail.com>
2021-01-07 14:54:40 -0800

    setup filter lesson for codesandbox

---
+++ rxjs-filtering-operator-filter/src/script.js
@@ -0,0 +1,15 @@
+var foo = Rx.Observable.interval(1000);
+
+/*
+--0--1--2--3--4--5--6--7-
+ filter(x => x % 2 === 0)
+--0-----2-----4-----6----
+*/
+
+var bar = foo.filter(x => x % 2 === 0);
+
+bar.subscribe(
+  function (x) { console.log('next ' + x)},
+  function (err) { console.log('error ' + err)},
+  function () { console.log('done')},
+);
