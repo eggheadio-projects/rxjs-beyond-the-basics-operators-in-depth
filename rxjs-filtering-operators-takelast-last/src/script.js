572a18b5e533e2f166a0e7b33964b26763f5a8c2
lsminter <lsminteris@gmail.com>
2021-01-07 15:58:10 -0800

    setup to work in codesandbox with latest dependencies

--- rxjs-filtering-operators-takelast-last/src/script.js
+++ rxjs-filtering-operators-takelast-last/src/script.js
@@ -1,8 +1,9 @@
 import { Observable } from "rxjs";
-import { skip, first, take } from "rxjs/operators";
+import { last, takeLast, take } from "rxjs/operators";
 import "rxjs/add/observable/interval";
 
-var foo = Observable.interval(1000).take(5);
+var foo = Observable.interval(1000);
+const takeFive = foo.pipe(take(5));
 
 /*
 --0--1--2--3--4--5--6--7-...
@@ -12,10 +13,16 @@ var foo = Observable.interval(1000).take(5);
 ---------------(4|)  (bar)
 */
 
-var bar = foo.last();
+var bar = takeFive.pipe(takeLast(2));
 
 bar.subscribe(
-  function (x) { console.log('next ' + x)},
-  function (err) { console.log('error ' + err)},
-  function () { console.log('done')},
+  function (x) {
+    console.log("next " + x);
+  },
+  function (err) {
+    console.log("error " + err);
+  },
+  function () {
+    console.log("done");
+  }
 );
