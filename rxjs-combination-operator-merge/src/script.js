784e1f14dc4a36d225297c9c4a91c39fd97cf8e2
lsminter <lsminteris@gmail.com>
2021-01-07 16:57:37 -0800

    updated code to work in codesandbox on latest dependencies

--- rxjs-combination-operator-merge/src/script.js
+++ rxjs-combination-operator-merge/src/script.js
@@ -1,14 +1,13 @@
-import { Observable, merge } from "rxjs";
-import { take, startWith } from "rxjs/operators";
-import "rxjs/add/observable/of";
-import "rxjs/add/observable/concat";
+import { Observable } from "rxjs";
+import { take } from "rxjs/operators";
+import "rxjs/add/observable/merge";
 import "rxjs/add/observable/interval";
 
-var foo = Observable.interval(500).take(4);
+var foo = Observable.interval(500);
 var takeFour = foo.pipe(take(4));
 
-var bar = Observable.interval(300).take(5);
-var takeFive = bar.pipe(take(4));
+var bar = Observable.interval(300);
+var takeFive = bar.pipe(take(5));
 
 /*
 ----0----1----2----(3|)     (foo)
@@ -17,7 +16,7 @@ var takeFive = bar.pipe(take(4));
 --0-01--21-3--(24)-(3|)
 */
 
-var merged = Observable.merge(fakeFour, fakeFive);
+var merged = Observable.merge(takeFour, takeFive);
 
 merged.subscribe(
   function (x) { console.log('next ' + x)},
