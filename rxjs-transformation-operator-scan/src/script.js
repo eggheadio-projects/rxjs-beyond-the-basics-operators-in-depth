6cb0e25b6ef97e50bf9ddd3c3c3d8d36ac91797d
lsminter <lsminteris@gmail.com>
2021-01-08 14:40:23 -0800

    initial setup for codesandbox for san operator lesson

---
+++ rxjs-transformation-operator-scan/src/script.js
@@ -0,0 +1,26 @@
+import { Observable } from "rxjs";
+import { take, withLatestFrom } from "rxjs/operators";
+import "rxjs/add/observable/of";
+import "rxjs/add/observable/zip";
+import "rxjs/add/observable/interval";
+
+var foo = Observable.of('h', 'e', 'l', 'l', 'o');
+var bar = Observable.interval(600)
+var takeFive = bar.pipe(take(5));
+
+/*
+(hello|)                          (foo)
+-----0-----1-----2-----3-----4|   (bar)
+       zip((x,y) => x)
+-----h-----e-----l-----l-----o|
+  scan((acc, x) => acc+x, '')
+-----h-----(he)--(hel)-(hell)(hello|)
+*/
+
+var combined = foo.zip(takeFive, (x,y) => x).scan((acc, x) => acc+x, '');
+
+combined.subscribe(
+  function (x) { console.log('next ' + x)},
+  function (err) { console.log('error ' + err)},
+  function () { console.log('done')},
+);
