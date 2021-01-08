c62de4221a6eb8f021ee44e51bcdb4d8a2fc7a07
lsminter <lsminteris@gmail.com>
2021-01-08 15:40:54 -0800

    WIP

--- rxjs-transformation-operator-buffer/src/script.js
+++ rxjs-transformation-operator-buffer/src/script.js
@@ -1,12 +1,17 @@
+//WIP
+
 import { Observable } from "rxjs";
-import { take, scan } from "rxjs/operators";
+import { take, buffer } from "rxjs/operators";
 import "rxjs/add/observable/of";
 import "rxjs/add/observable/zip";
 import "rxjs/add/observable/interval";
 
-var foo = Observable.of('h', 'e', 'l', 'l', 'o')
-  .zip(Observable.interval(600).take(5), (x,y) => x);
-var bar = Observable.interval(900).take(3);
+var foo = Observable.interval(('h', 'e', 'l', 'l', 'o'), 600);
+var fooZip = Observable.zip(foo);
+var fooTakeFive = fooZip.pipe(take(5));
+
+var bar = Observable.interval(900)
+var barTakeThree = bar.pipe(take(3));
 
 /*
 -----h-----e-----l-----l-----o|       (foo)
@@ -17,7 +22,7 @@ var bar = Observable.interval(900).take(3);
 --------h--------e--------ll|
 */
 
-var result = foo.buffer(bar);
+var result = fooTakeFive.pipe(buffer(barTakeThree));
 
 result.subscribe(
   function (x) { console.log('next ' + x)},
