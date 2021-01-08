22b23e959f4ee5c2fd5cb88919c9452d7ccebc32
lsminter <lsminteris@gmail.com>
2021-01-07 17:24:35 -0800

    got working in codesandbox, need to get final function to work

--- rxjs-combination-operator-combinelatest/src/script.js
+++ rxjs-combination-operator-combinelatest/src/script.js
@@ -1,6 +1,7 @@
-import { Observable } from "rxjs";
+// WIP
+
+import { Observable, combineLatest } from "rxjs";
 import { take } from "rxjs/operators";
-import "rxjs/add/observable/merge";
 import "rxjs/add/observable/interval";
 
 var foo = Observable.interval(500);
@@ -9,7 +10,6 @@ var takeFour = foo.pipe(take(4));
 var bar = Observable.interval(300);
 var takeFive = bar.pipe(take(5));
 
-
 /*
 ----0----1----2----(3|)     (weight)
 --0--1--2--3--(4|)          (height)
@@ -17,13 +17,22 @@ var takeFive = bar.pipe(take(5));
 ----01--23-4--(56)-(7|)
 */
 
-var combined = takeFour.combineLatest(takeFive,(x,y) => x+y);
+// I'm unsure if this is possible anymore 
+// var combined = takeFour.pipe(combineLatest(takeFive, (x, y) => x + y));
+
+var combined = combineLatest(takeFour, takeFive, (x,y) => x+y);
 
 // merge: OR
 // combineLatest: AND
-                               
+
 combined.subscribe(
-  function (x) { console.log('next ' + x)},
-  function (err) { console.log('error ' + err)},
-  function () { console.log('done')},
+  function (x) {
+    console.log("next " + x);
+  },
+  function (err) {
+    console.log("error " + err);
+  },
+  function () {
+    console.log("done");
+  }
 );
