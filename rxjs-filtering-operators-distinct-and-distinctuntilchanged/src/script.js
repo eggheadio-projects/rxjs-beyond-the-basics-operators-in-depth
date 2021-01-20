1e604ad16ddaee6c21490f6e6201ca1f60183f9c
lsminter <lsminteris@gmail.com>
2021-01-20 14:23:09 -0800

    updated code for distinctuntilchanged lesson 18 to be using latest dependencies

--- rxjs-filtering-operators-distinct-and-distinctuntilchanged/src/script.js
+++ rxjs-filtering-operators-distinct-and-distinctuntilchanged/src/script.js
@@ -1,12 +1,18 @@
 import { Observable } from "rxjs";
-import { take, debounce, debounceTime } from "rxjs/operators";
+import { take, distinctUntilChanged } from "rxjs/operators";
 import "rxjs/add/observable/interval";
 import "rxjs/add/observable/of";
 import "rxjs/add/observable/zip";
 
-var foo = Observable.interval(500)
-var fooTakeFive = foo.pipe(take(5))
-var fooZip = Observable.zip(Rx.Observable.of('a','b','a','a','b'), (x,y)=>y);
+var foo = Observable.of('a', 'b', 'a', 'a', 'b')
+var fooInterval = Observable.interval(500);
+var fooTakeFive = fooInterval.pipe(take(5));
+var fooCombined = Observable.zip(foo, fooTakeFive, (x)=>x);
+
+
+// var foo = Observable.interval(500)
+// var fooTakeFive = foo.pipe(take(5))
+// var fooZip = fooTakeFive.pipe(zip(Observable.of('a', 'b', 'a', 'a', 'b'), (x,y)=>y));
 
 /*
 --a--b--a--a--b|
@@ -14,7 +20,7 @@ var fooZip = Observable.zip(Rx.Observable.of('a','b','a','a','b'), (x,y)=>y);
 --a--b--a-----b|
 */
 
-var result = foo.distinctUntilChanged();
+var result = fooCombined.pipe(distinctUntilChanged());
 
 result.subscribe(
   function (x) { console.log('next ' + x)},
