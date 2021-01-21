ccb8745f38023eb16618818d999f17c53c2f277a
lsminter <lsminteris@gmail.com>
2021-01-20 16:29:19 -0800

    updated code to work with latest dependencies and codesandbox

--- rxjs-transformation-operator-map-and-mapto/src/script.js
+++ rxjs-transformation-operator-map-and-mapto/src/script.js
@@ -1,5 +1,5 @@
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, mapTo } from "rxjs/operators";
 import "rxjs/add/observable/interval";
 
 var foo = Observable.interval(1000);
@@ -12,7 +12,10 @@ bar: ---0---2---4---6--...
 
 */
 
-var bar = foo.pipe(map(x => x / 10));
+var bar = foo.pipe(mapTo(10))
+
+// uncomment below to see map example
+// var bar = foo.pipe(map(x => x / 2));
 
 bar.subscribe(
   function (x) { console.log('next ' + x)},
