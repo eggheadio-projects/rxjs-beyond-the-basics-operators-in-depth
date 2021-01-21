5dfca14fbe7499599fb51a876b185396fe64a23e
lsminter <lsminteris@gmail.com>
2021-01-20 16:10:46 -0800

    updated lesson 20 code to work with latest dependencies and codesandbox

--- rxjs-error-handling-operator-retry-and-retrywhen/src/script.js
+++ rxjs-error-handling-operator-retry-and-retrywhen/src/script.js
@@ -1,7 +1,17 @@
-var foo = Rx.Observable.interval(500)
-  .zip(Rx.Observable.of('a','b','c','d',2), (x,y)=>y);
+import { Observable } from "rxjs";
+import { map, retryWhen, delay } from 'rxjs/operators';
+// uncomment below to see retry operator
+// import { retry } from 'rxjs/operators';
+import "rxjs/add/observable/interval";
+import "rxjs/add/observable/of";
+import "rxjs/add/observable/zip";
 
-var bar = foo.map(x => x.toUpperCase());
+var foo = Observable.of('a', 'b', 'c', 'd', 2)
+var fooInterval = Observable.interval(500);
+var fooCombined = Observable.zip(foo, fooInterval, (x)=>x);
+
+
+var bar = fooCombined.pipe(map(x => x.toUpperCase()));
 
 /*
 --a--b--c--d--2|     (foo)
@@ -11,20 +21,13 @@ map(toUpperCase)
 --A--B--C--D-----------A--B--C--D-------------A--B--C--D---
 */
 
-var result = bar.retryWhen(errorObs => errorObs.delay(3000));
+// uncommet below to see retry operator
+// var result = bar.pipe(retry(2));
+
+var result = bar.pipe(retryWhen(errorObs => errorObs.pipe(delay(3000))));
 
 result.subscribe(
-  function (x) { console.log('next ' + x) || displayInPreview('next ' + x); },
-  function (err) { console.log('error ' + err) || displayInPreview('error ' + err); },
-  function () { console.log('done') || displayInPreview('done'); },
+  function (x) { console.log('next ' + x)},
+  function (err) { console.log('error ' + err)},
+  function () { console.log('done')},
 );
-
-
-
-// display in plunker preview
-function displayInPreview(string) {
-  var newDiv = document.createElement("div"); 
-  var newContent = document.createTextNode(string); 
-  newDiv.appendChild(newContent);
-  document.body.appendChild(newDiv)
-}
